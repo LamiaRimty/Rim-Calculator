51307ccb229b5e3b6574ece82e25febd986979dd
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-09 11:37:58 +0200

    Create html files

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const calculator = express();
-calculator.listen(3000);
+calculator.listen(3000,function(){
+    console.log("Server is running on port 3000");
+    });
 
 calculator.get("/",function(req,res){
   res.send("<h1>Hello World</h1>");
