07fda36e59393878690d29238f9c519700d65301
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-09 12:39:41 +0200

    Response to Request with html files

--- calculator.js
+++ calculator.js
@@ -1,9 +1,12 @@
+//jshint esversion:6
 const express = require("express");
 const calculator = express();
+
+calculator.get("/",function(req,res){
+    res.sendFile(__dirname+ "/index.html");
+  });
+
 calculator.listen(3000,function(){
     console.log("Server is running on port 3000");
     });
 
-calculator.get("/",function(req,res){
-  res.send("<h1>Hello World</h1>");
-})
