5b9b11f39e2d6ee12215ec9b97a0d1d0bb0c0cfd
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-10 17:24:13 +0200

    Processing post request

--- calculator.js
+++ calculator.js
@@ -1,17 +1,24 @@
 //jshint esversion:6
 const express = require("express");
-const bodyParser= require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extented: true}));
+app.use(bodyParser.urlencoded({ extended:true }));
+//app.use(bodyParser.urlencoded({extented: true})); //allows us to post nested obj,its bdyprsr requiring u
+
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+ "/index.html");
   });
 
-  calculator.post("/",function(req,res){
+ app.post("/",function(req,res){
+ var num1=Number(req.body.n1);
+ var num2=Number(req.body.n2);
+ var result=num1+num2;
+ console.log(result);
     res.send("Thanks for posting");
-  });
+  
+});
 
 
 app.listen(3000,function(){
